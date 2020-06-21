4580e4afb4d953deb6c46de581650843e9a441e8
Shariq Bin Shoaib <sbs_ansari@outlook.com>
2020-06-22 02:34:05 +0500

    remove code specified for production

--- index.js
+++ index.js
@@ -7,15 +7,5 @@ require("./startup/config")();
 require("./startup/db")();
 require("./startup/routes")(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/dist/examination"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client", "dist", "examination", "index.html")
-    );
-  });
-}
-
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
