b3c4766914a42e9b0d64401bf3ee96d26d4d8759
Shariq Bin Shoaib <sbs_ansari@outlook.com>
2020-07-05 12:36:07 +0500

    added log to file

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,4 +1,7 @@
+const logger = require("../utils/logger");
+
 module.exports = function (err, req, res, next) {
   console.log("Error Middleware: ", err);
+  logger.info(err);
   res.status(500).send("Something Failed");
 };
