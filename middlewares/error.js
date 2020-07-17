23e49f13587f62065869a3ef68f5af5f7fb393ea
Shariq Bin Shoaib <sbs_ansari@outlook.com>
2020-07-18 00:29:06 +0500

    error message changed

--- middlewares/error.js
+++ middlewares/error.js
@@ -3,5 +3,5 @@ const logger = require("../utils/logger");
 module.exports = function (err, req, res, next) {
   console.log("Error Middleware: ", err);
   logger.info(err);
-  res.status(500).send("Something Failed");
+  res.status(500).send("An unexpected error occured!");
 };
