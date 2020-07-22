d600528285783fa2e2d1f06b1159d63b09af4491
Shariq Bin Shoaib <sbs_ansari@outlook.com>
2020-07-23 01:52:46 +0500

    code refactored by using createAsyncAction func

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,46 +1,20 @@
 import http from "../services/httpService";
 import auth from "../services/authService";
 import { saveUser } from "../services/userService";
-import { setLoading, unSetLoading } from "./uiActions";
 import { SET_CURRENT_USER } from "./types";
 import toastify from "../utils/toastNotification";
+import createAsyncAction from "../utils/createAsyncAction";
 
 // Register User
-export const registerUser = (user) => (dispatch) => {
-  dispatch(setLoading());
-  saveUser(user).then(
-    (res) => {
-      toastify(res.data, "success");
-      dispatch(unSetLoading());
-    },
-    (err) => {
-      if (err.response && err.response.status === 400) {
-        toastify(err.response.data, "error");
-      }
-      dispatch(unSetLoading());
-    }
-  );
-};
+export const registerUser = createAsyncAction(saveUser, (succcessResponse) => {
+  toastify(succcessResponse.data, "success");
+  setTimeout(() => (window.location = "/login"), 3000);
+});
 
 // Login User
-export const loginUser = ({ email, password }) => (dispatch) => {
-  dispatch(setLoading());
-  auth.login(email, password).then(
-    (res) => {
-      dispatch(unSetLoading());
-      window.location = "/user";
-    },
-    (err) => {
-      if (
-        (err.response && err.response.status === 400) ||
-        err.response.status === 401
-      ) {
-        toastify(err.response.data, "error");
-      }
-      dispatch(unSetLoading());
-    }
-  );
-};
+export const loginUser = createAsyncAction(auth.login, () => {
+  window.location = "/user";
+});
 
 // Set logged in User
 export const setCurrentUser = (user) => ({
@@ -50,11 +24,11 @@ export const setCurrentUser = (user) => ({
 
 // LogOut User
 export const logoutUser = () => (dispatch) => {
-  auth.logout();
-
   // Remove jwt token from http headers
   http.setJwt(false);
 
   // Set current user to empty object
   dispatch(setCurrentUser({}));
+
+  auth.logout();
 };
