6423e1a0eb74d6fba4f257cb334c5bdea1bf1b23
Shariq Bin Shoaib <sbs_ansari@outlook.com>
2020-07-27 23:59:48 +0500

    configured for production

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
+const path = require("path");
+const compression = require("compression");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+if (!isProduction) {
+  require("dotenv").config();
+}
+
+// gzip compression of responses
+app.use(compression());
+
+/* Redirect http to https */
+app.get("*", function (req, res, next) {
+  if (req.headers["x-forwarded-proto"] != "https" && isProduction)
+    res.redirect("https://" + req.hostname + req.url);
+  else next(); /* Continue to other routes if we're not redirecting */
+});
 
 require("./startup/logging")();
 require("./startup/config")();
 require("./startup/db")();
 require("./startup/routes")(app);
+
+// if (isProduction) {
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+// }
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
